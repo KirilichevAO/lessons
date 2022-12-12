88b0d0a36bdbc48721a20a2126c72b5e6450faf2
KirilichevAO <kirilichev.a@icloud.com>
2022-12-12 16:44:22 +0700

    12.12.2022

---
+++ 3.1.py
@@ -0,0 +1,133 @@
+# def calc1(x):
+#     return x + 10
+
+# #print(calc1(10))
+
+
+# def calc2(x):
+#     return x*10
+
+# #print(calc2(10))
+
+
+# # сворачиваем вверху преведенные функции в функцию
+# def math(op, x):
+#     print(op(x))
+
+# math(calc1, 10)
+# math(calc2, 10)
+#####################################################
+
+
+# # усложняем функцию и повторяем то же самое
+# def sum(x, y):
+#     return x + y
+
+
+# def mult(x, y):
+#     return x * y
+
+
+# def calc(op, a, b):
+#     print(op(a, b))
+# #    return op(a, b)
+
+
+# calc(mult, 4, 5)
+#####################################################
+
+
+############
+## lambda ##
+############
+
+# # переписываем верхние функции по новому синтаксису
+# sum = lambda x, y: x + y
+# mult = lambda x, y: x * y
+# calc = lambda op, a, b: op(a, b)
+# print(calc(mult, 4, 5))
+#####################################################
+
+
+# # делаем еще проще
+# sum = lambda x, y: x + y
+# #mult = lambda x, y: x * y # вставляем в нижнюю тем самым сокращаем код
+# calc = lambda op, a, b: op(a, b)
+# #print(calc(mult, 4, 5)) # было
+# print(calc(lambda x, y: x * y, 4, 5)) # стало
+#####################################################
+
+########################
+## List Comprehension ##
+########################
+
+# list = [] # было
+# for i in range(1, 21):
+#     list.append(i)
+
+# print(list)
+
+
+# list = [i for i in range(1, 21)] # стало
+# print(list)
+#####################################################
+
+
+# list = [] # было
+# for i in range(1, 21):
+#     if (i % 2 == 0):
+#         list.append(i)
+
+# print(list)
+
+
+# list = [i for i in range(1, 21) if i % 2 == 0] # стало
+# print(list)
+
+
+# list = [(i, i) for i in range(1, 21) if i % 2 == 0] # с кортежами
+# print(list)
+#####################################################
+
+
+# def f(x):
+#     return x**3
+
+# list = [f(i) for i in range(1, 21) if i % 2 == 0]
+# print(list)
+
+
+# def f(x):
+#     return x**3
+
+# list = [(i, f(i)) for i in range(1, 21) if i % 2 == 0] # с кортежами
+# print(list)
+#####################################################
+
+
+# f = open('f.txt', 'r') # было
+# data = f.read() + ' '
+# f.close()
+
+# numbers = []
+
+# while data != '':
+#      space_pos = data.index(' ')
+#      numbers.append(int(data[:space_pos]))
+#      data = data[space_pos+1:]
+# out = []
+# for e in numbers:
+#      if not e % 2:
+#          out.append((e, e **2 ))
+# print(out)
+
+
+def select(f, col): # стало
+    return [f(x) for x in col]
+def where(f, col):
+    return [x for x in col if f(x)]
+data = '1 2 3 5 8 15 23 38'.split()
+data = select(int, data)
+data = where(lambda e: not e % 2, data)
+data = list(select(lambda e: (e, e**2), data))
+ 
